1e28335abd2a000532af0e7cc89ddc430403df89
Pedro-Hryk <vidz0679@gmail.com>
2023-05-29 21:09:59 -0300

    140 - Added create function for comment.

---
+++ IdentityApp/Controllers/CommentController.cs
@@ -0,0 +1,42 @@
+﻿using IdentityApp.Services;
+using IdentityEcommerce.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityApp.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly CommentService _commentService;
+        private readonly UserManager<AppUser> _userManager;
+
+        public CommentController(CommentService commentService, UserManager<AppUser> userManager)
+        {
+            _commentService = commentService;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Create(int productID, int reviewID)
+        {
+            var userID = _userManager.GetUserId(HttpContext.User);
+            var comment = new Comment();
+            comment.ProductID = productID;
+            comment.ReviewID = reviewID;
+            comment.UserId = int.Parse(userID);
+            return View(comment);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Comment comment)
+        {
+            bool createdComment = _commentService.Create(comment);
+            if (createdComment)
+            {
+                return RedirectToAction("Details", "Product", new {productID = comment.ProductID});
+            }
+            return View();
+        }
+
+    }
+}
