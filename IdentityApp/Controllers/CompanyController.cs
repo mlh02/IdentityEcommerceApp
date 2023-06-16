457faf28f50e387020628ccaddaaa3fd4b03f2e2
Pedro-Hryk <vidz0679@gmail.com>
2023-06-16 16:52:58 -0300

    210- Added admin and company create form.

--- IdentityApp/Controllers/CompanyController.cs
+++ IdentityApp/Controllers/CompanyController.cs
@@ -1,18 +1,24 @@
-﻿using IdentityEcommerce.Helpers.Enums;
+﻿using IdentityApp.Models.ViewModels;
+using IdentityEcommerce.Helpers.Enums;
 using IdentityEcommerce.Models;
 using IdentityEcommerce.Models.Repositories;
 using IdentityEcommerce.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace IdentityEcommerce.Controllers
 {
     public class CompanyController : Controller
     {
         private readonly CompanyService _companiesService;
+        private readonly UserManager<AppUser> _userManager;
 
-        public CompanyController(CompanyService companiesService)
+        public CompanyController(CompanyService companiesService, UserManager<AppUser> userManager)
         {
             _companiesService = companiesService;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -24,7 +30,34 @@ namespace IdentityEcommerce.Controllers
             }
             return RedirectToAction("Login", "AppUser");
         }
+        [HttpGet]
+        public IActionResult RegisterAdmin()
+        {
+            var adminAndCompanyViewModel = new AdminAndCompanyFormViewModel();
+            return View(adminAndCompanyViewModel);
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> RegisterAdmin(AdminAndCompanyFormViewModel adminAndCompanyFormViewModel)
+        {
+            //registering admin
+            var role = AppRoleEnum.SuperUser.ToString();
+            var newAdmin = adminAndCompanyFormViewModel.AdminForm;
+            var adminRegister = await _userManager.CreateAsync(newAdmin);
+            var assignRole = await _userManager.AddToRoleAsync(newAdmin, role);
+            newAdmin.IsAdmin = true;
+
+            //registering company
+            var newCompany = adminAndCompanyFormViewModel.CompanyForm;
+            bool createdCompany = _companiesService.Create(newCompany);
+            if (createdCompany)
+            {
+                newAdmin.AssignedCompanyId = newCompany.CompanyID.ToString();
+                await _userManager.UpdateAsync(newAdmin);
+                return RedirectToAction("Index", "Product");
+            }
+            return View();
+        }
         [HttpGet]
         public IActionResult Create()
         {
@@ -47,6 +80,7 @@ namespace IdentityEcommerce.Controllers
             _companiesService.Delete(companyID);
             return RedirectToAction("Index");
         }
+
     }
 
 }
---
+++ IdentityApp/Models/ViewModels/AdminAndCompanyFormViewModel.cs
@@ -0,0 +1,10 @@
+﻿using IdentityEcommerce.Models;
+
+namespace IdentityApp.Models.ViewModels
+{
+    public class AdminAndCompanyFormViewModel
+    {
+        public AppUser AdminForm { get; set; }
+        public Company CompanyForm { get; set; }
+    }
+}
